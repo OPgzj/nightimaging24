314914b89c38357e6a5c6189b04e2bb584103e52
juicemachineopgzj <1363328344@qq.com>
2025-02-16 14:42:17 +0800

    添加注释

--- raw_prc_pipeline/crop_resize.py
+++ raw_prc_pipeline/crop_resize.py
@@ -1,11 +1,17 @@
 import cv2
 import numpy as np
 
-
+'''
+根据huawei bounds进行裁剪
+'''
 def crop_by_bounds(image: np.ndarray, bounds_coords: tuple) -> np.ndarray:
     h_start, h_end, w_start, w_end = bounds_coords
     return image[h_start:h_end, w_start:w_end]
 
+
+'''
+垂直裁剪[200:2200],水平居中裁剪
+'''
 
 def upper_crop(img, upper_bound = 200):
     mid = img.shape[1] // 2
@@ -21,6 +27,6 @@ def crop_resize(undistorted_img: np.ndarray, bounds: tuple[int, int, int, int]):
     Returns:
         np.ndarray: Cropped and resized image.
     """
-    resized = cv2.resize(undistorted_img, dsize=(2654, 3538)) 
+    resized = cv2.resize(undistorted_img, dsize=(2654, 3538)) # 压缩图像
     cropped = upper_crop(crop_by_bounds(resized, bounds))
     return cropped
--- raw_prc_pipeline/fs.py
+++ raw_prc_pipeline/fs.py
@@ -1,7 +1,21 @@
 import cv2
 import numpy as np
 
-
+'''
+作用：
+该函数用于模拟“闪光”效果，增强图像的亮度和对比度。
+参数：
+source：输入图像。
+a：控制增强强度的参数，默认为5。
+target：目标亮度，默认为-1，表示自动调整。
+perform_gamma_correction：是否进行伽马校正，默认为True。
+处理流程：
+计算输入图像的亮度通道（取RGB三通道的最大值）。
+防止亮度为0的情况，将0值替换为极小值（1e-9）。
+根据亮度值和参数a计算调整后的像素值。
+如果启用了伽马校正，对结果进行伽马校正。
+根据目标亮度target调整结果图像的亮度，如果没有指定目标亮度，则将最大亮度调整为255。
+'''
 def perform_flash(source, a=5, target=-1, perform_gamma_correction=True):
     rows, cols, _ = source.shape
 
@@ -21,6 +35,24 @@ def perform_flash(source, a=5, target=-1, perform_gamma_correction=True):
     return result
 
 
+'''
+作用：
+该函数用于实现“风暴”效果，通过多尺度亮度调整增强图像的细节和对比度。
+参数：
+source：输入图像。
+a：控制增强强度的参数，默认为5。
+target：目标亮度，默认为-1，表示自动调整。
+kernels：一组用于多尺度亮度调整的核大小。
+perform_gamma_correction：是否进行伽马校正，默认为True。
+处理流程：
+计算输入图像的亮度通道（取RGB三通道的最大值）。
+防止亮度为0的情况，将0值替换为极小值（1e-9）。
+对亮度值进行对数变换。
+使用不同大小的核进行盒式滤波，模拟多尺度亮度调整。
+根据滤波后的亮度值和参数a计算调整后的像素值。
+如果启用了伽马校正，对结果进行伽马校正。
+根据目标亮度target调整结果图像的亮度，如果没有指定目标亮度，则将最大亮度调整为255。
+'''
 def perform_storm(source, a=5, target=-1, kernels=(1, 4, 16, 64, 256), perform_gamma_correction=True):
     rows, cols, _ = source.shape
 
